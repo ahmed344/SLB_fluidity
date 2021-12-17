7b53113bd1387dad39705c4afa4d259ccae53d3e
Ahmed <ahmed@pc-ahmedGTX.cinam.univ-mrs.fr>
2021-12-17 19:45:41 +0100

    fix some mistakes

--- slb.py
+++ slb.py
@@ -19,20 +19,24 @@ class IntensityCurve():
     def curve_fitting(self, show=False):
         """Fit the intesity curve on the data.
         Parameter:
-            t, x, It, Ix - as numpy arrays
+            t, x, It, Ix                 - as numpy arrays
         Returns: 
-            D, B, Io, Ib - of the intensity equations.
+            B, Io_t, Ib_t, D, Io_x, Ib_x - of the intensity equations.
         """
         
         # Apply the fitting 
-        (B, Io, Ib), _ = curve_fit(IntensityCurve.intensity_time, self.t, self.It, p0=(0, self.It.max(), self.It.max()/3))
+        (B, Io_t, Ib_t), _ = curve_fit(IntensityCurve.intensity_time, self.t, self.It,
+                                       p0=(0, self.It.max(), self.It.max()/3))
+        if show == True: print(f'Intensity equation of time:\nIo = {Io_t:.3f}\nIb = {Ib_t:.3f}\nB = {B:.3f}\n')
         
         # Intesity of the rectangle as a function of space from the edge
-        def intensity_space(x, D):
+        def intensity_space(x, D, Io, Ib):
             return Io * np.exp(-x*np.sqrt(B/D)) + Ib
         
-        D, _ = curve_fit(intensity_space, self.x, self.Ix, p0=1, bounds=[1e-100,np.inf])
-        D = D[0]
+        (D, Io_x, Ib_x), _ = curve_fit(intensity_space, self.x, self.Ix,
+                                       p0=(1, self.It.max(),self.It.max()/3),
+                                       bounds=([1e-100,0,0],[np.inf, np.inf, np.inf]))
+        if show == True: print(f'Intensity equation of space:\nIo = {Io_x:.3f}\nIb = {Ib_x:.3f}\nD = {D:.3f}')
         
         # Display the results
         if show == True:
@@ -41,21 +45,23 @@ class IntensityCurve():
             # Intensity time curve
             plt.subplot(121)
             plt.plot(self.t, self.It, label='Data')
-            plt.plot(self.t, Io * np.exp(-B*self.t) + Ib, "r--", label =f'fit: Io={Io:.3f}, Ib={Ib:.3f}, B={B:.3f}')
-            plt.title('I(t)')
-            plt.xlabel('Time [S]')
-            plt.ylabel('I(t)')
+            plt.plot(self.t, Io_t * np.exp(-B*self.t) + Ib_t, "r--",
+                     label =f'Fitting:\n$I_o$={Io_t:.3f}\n$I_b$={Ib_t:.3f}\n$B$={B:.3f} $1/s$')
+            plt.title('$I(t) = I_0 e^{(-Bt)} + I_{b}$', fontsize='xx-large')
+            plt.xlabel('Time [S]', fontsize='large')
+            plt.ylabel('I(t)', fontsize='large')
             plt.grid()
-            plt.legend()
+            plt.legend(fontsize='large')
 
             # Intensity space curve
             plt.subplot(122)
             plt.plot(self.x, self.Ix, label='Data')
-            plt.plot(self.x, Io * np.exp(-self.x*np.sqrt(B/D)) + Ib, "r--", label =f'fit: D={D:.4f}')
-            plt.title('I(x)');
-            plt.xlabel('Distance [ $\mu m^2$ ]')
-            plt.ylabel('I(x)')
+            plt.plot(self.x, Io_x * np.exp(-self.x*np.sqrt(B/D)) + Ib_x, "r--",
+                     label =f'Fitting:\n$I_o$={Io_x:.3f}\n$I_b$={Ib_x:.3f}\n$D$={D:.3f} $\mu m^2/s$')
+            plt.title('$I(x) = I_0 e^{-x \sqrt{B/D}} + I_{b}$', fontsize='xx-large')
+            plt.xlabel('Distance [ $\mu m^2$ ]', fontsize='large')
+            plt.ylabel('I(x)', fontsize='large')
             plt.grid()
-            plt.legend();
+            plt.legend(fontsize='large');
 
-        return B, Io, Ib, D # B, Io, Ib, D of the intensity equations
+        return B, Io_t, Ib_t, D, Io_x, Ib_x # B, Io, Ib, D of the intensity equations
